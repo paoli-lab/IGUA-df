306d9fbeb256f83af5a040370cf9d91c34604e1d
sedaradoykova <seda.radoykova@gmail.com>
2025-05-14 12:09:33 +0200

    refactor to BaseDataset (only seqio)

--- igua/seqio.py
+++ igua/seqio.py
@@ -1,3 +1,4 @@
+import abc
 import io
 import typing
 import pathlib
@@ -12,79 +13,166 @@ import rich.progress
 _GZIP_MAGIC = b'\x1f\x8b'
 
 
-def write_fasta(file: typing.TextIO, name: str, sequence: str) -> None:
-    file.write(">{}\n".format(name))
-    file.write(sequence)
-    file.write("\n")
+class BaseDataset(abc.ABC):
+    """Base class for dataset extraction.
+    This class defines the basic structure and methods for extracting nucleotide and
+    protein sequences from various file formats. It serves as a base class for specific 
+    dataset classes like GenBankDataset and GFFDataset.
+    """
+    @abc.abstractmethod
+    def extract_sequences(
+        self,
+        progress: rich.progress.Progress,
+        inputs: typing.List[pathlib.Path],
+        output: pathlib.Path,
+        ) -> pandas.DataFrame:
+        pass
+    
+    @abc.abstractmethod
+    def extract_proteins(
+        self,
+        progress: rich.progress.Progress,
+        inputs: typing.List[pathlib.Path],
+        output: pathlib.Path,
+        representatives: typing.Container[str]
+        ) -> typing.Dict[str, int]:
+        pass
+    
+    def write_fasta(self, file: typing.TextIO, name: str, sequence: str) -> None:
+        file.write(">{}\n".format(name))
+        file.write(sequence)
+        file.write("\n")
+        return None
+
+    def translate_orf(self, sequence: typing.Union[str, bytes], translation_table: int = 11) -> str:
+        return str(Bio.Seq.Seq(sequence).translate(translation_table))
 
 
-def extract_sequences(
-    progress: rich.progress.Progress,
-    inputs: typing.List[pathlib.Path],
-    output: pathlib.Path,
-) -> pandas.DataFrame:
-    data = []
-    done = set()
-    n_duplicate = 0
-    with open(output, "w") as dst:
-        task1 = progress.add_task(f"[bold blue]{'Working':>9}[/]")
-        for input_path in progress.track(inputs, task_id=task1):
-            task2 = progress.add_task(f"[bold blue]{'Reading':>9}[/]")
-            with io.BufferedReader(progress.open(input_path, "rb", task_id=task2)) as reader:  # type: ignore
-                if reader.peek().startswith(_GZIP_MAGIC):
-                    reader = gzip.GzipFile(mode="rb", fileobj=reader)  # type: ignore
-                for record in gb_io.iter(reader):
-                    if record.name in done:
-                        n_duplicate += 1
-                    else:
-                        write_fasta(dst, record.name, record.sequence.decode("ascii"))
-                        data.append((record.name, len(record.sequence), input_path))
-                        done.add(record.name)
-            progress.remove_task(task2)
-        progress.remove_task(task1)
-    if n_duplicate > 0:
+class GenBankDataset(BaseDataset):
+    """GenBank dataset class.
+    This class is used to extract nucleotide and protein sequences from GenBank files.
+    It inherits from the BaseDataset class and implements the extract_sequences
+    and extract_proteins methods.
+    """
+    def extract_sequences(
+        self,
+        progress: rich.progress.Progress,
+        inputs: typing.List[pathlib.Path],
+        output: pathlib.Path,
+    ) -> pandas.DataFrame:
+        """Extracts nucleotide sequences from GenBank files.
+        Args:
+            progress (rich.progress.Progress): Progress bar for tracking progress.
+            inputs (typing.List[pathlib.Path]): List of input GenBank files.
+            output (pathlib.Path): Output file path for the extracted sequences. 
+        Returns:
+            pandas.DataFrame: DataFrame containing the extracted sequences.
+        """
+        data = []
+        done = set()
+        n_duplicate = 0
+        with open(output, "w") as dst:
+            task1 = progress.add_task(f"[bold blue]{'Working':>9}[/]")
+            for input_path in progress.track(inputs, task_id=task1):
+                task2 = progress.add_task(f"[bold blue]{'Reading':>9}[/]")
+                with io.BufferedReader(progress.open(input_path, "rb", task_id=task2)) as reader:  # type: ignore
+                    if reader.peek().startswith(_GZIP_MAGIC):
+                        reader = gzip.GzipFile(mode="rb", fileobj=reader)  # type: ignore
+                    for record in gb_io.iter(reader):
+                        if record.name in done:
+                            n_duplicate += 1
+                        else:
+                            self.write_fasta(dst, record.name, record.sequence.decode("ascii"))
+                            data.append((record.name, len(record.sequence), input_path))
+                            done.add(record.name)
+                progress.remove_task(task2)
+            progress.remove_task(task1)
+        if n_duplicate > 0:
+            progress.console.print(
+                f"[bold yellow]{'Skipped':>12}[/] {n_duplicate} clusters with duplicate identifiers"
+            )
+        return pandas.DataFrame(
+            data=data,
+            columns=["cluster_id", "cluster_length", "filename"]
+        ).set_index("cluster_id")
+
+    
+    def extract_proteins(
+        self,
+        progress: rich.progress.Progress,
+        inputs: typing.List[pathlib.Path],
+        output: pathlib.Path,
+        representatives: typing.Container[str],
+    ) -> typing.Dict[str, int]:
+        """Extracts protein sequences from GenBank files.
+        Args:
+            progress (rich.progress.Progress): Progress bar for tracking progress.
+            inputs (typing.List[pathlib.Path]): List of input GenBank files.
+            output (pathlib.Path): Output file path for the extracted protein sequences.
+            representatives (typing.Container[str]): Set of representative cluster IDs.
+        Returns:
+            typing.Dict[str, int]: Dictionary containing protein IDs and their sizes. 
+        """
+        protein_sizes = {}
+        with output.open("w") as dst:
+            for input_path in inputs:
+                task = progress.add_task(f"[bold blue]{'Reading':>9}[/]")
+                with io.BufferedReader(progress.open(input_path, "rb", task_id=task)) as reader:  # type: ignore
+                    if reader.peek()[:2] == b'\x1f\x8b':
+                        reader = gzip.GzipFile(mode="rb", fileobj=reader)  # type: ignore
+                    for record in gb_io.iter(reader):
+                        if record.name in representatives:
+                            for i, feat in enumerate(
+                                filter(lambda f: f.kind == "CDS", record.features)
+                            ):
+                                qualifier = next((qualifier for qualifier in feat.qualifiers if qualifier.key == "translation"), None)
+                                if qualifier is None:
+                                    rich.print(f"[bold yellow]{'Warning':>12}[/] no 'translation' qualifier found in CDS feature of {record.name!r}")
+                                    translation = self.translate_orf(record.sequence[feat.location.start:feat.location.end])
+                                else:
+                                    translation = qualifier.value.rstrip("*")
+                                protein_id = "{}_{}".format(record.name, i)
+                                if protein_id not in protein_sizes:
+                                    self.write_fasta(dst, protein_id, translation)
+                                    protein_sizes[protein_id] = len(translation)
+                progress.remove_task(task)
         progress.console.print(
-            f"[bold yellow]{'Skipped':>12}[/] {n_duplicate} clusters with duplicate identifiers"
+            f"[bold green]{'Extracted':>12}[/] {len(protein_sizes)} proteins from {len(representatives)} nucleotide representative"
         )
-    return pandas.DataFrame(
-        data=data,
-        columns=["cluster_id", "cluster_length", "filename"]
-    ).set_index("cluster_id")
+        return protein_sizes
 
 
-def translate_orf(sequence: typing.Union[str, bytes], translation_table: int = 11) -> str:
-    return str(Bio.Seq.Seq(sequence).translate(translation_table))
+class GFFDataset(BaseDataset):
+    """GFF dataset class.
+    This class is used to extract nucleotide and protein sequences from GFF files.
+    It inherits from the BaseDataset class and implements the extract_sequences
+    and extract_proteins methods.
+    """
+    def __init__(self, gff: typing.Optional[typing.Union[str, pathlib.Path]] = None):
+        """Initializes the GFFDataset class.
 
-def extract_proteins(
-    progress: rich.progress.Progress,
-    inputs: typing.List[pathlib.Path],
-    output: pathlib.Path,
-    representatives: typing.Container[str],
-) -> typing.Dict[str, int]:
-    protein_sizes = {}
-    with output.open("w") as dst:
-        for input_path in inputs:
-            task = progress.add_task(f"[bold blue]{'Reading':>9}[/]")
-            with io.BufferedReader(progress.open(input_path, "rb", task_id=task)) as reader:  # type: ignore
-                if reader.peek()[:2] == b'\x1f\x8b':
-                    reader = gzip.GzipFile(mode="rb", fileobj=reader)  # type: ignore
-                for record in gb_io.iter(reader):
-                    if record.name in representatives:
-                        for i, feat in enumerate(
-                            filter(lambda f: f.kind == "CDS", record.features)
-                        ):
-                            qualifier = next((qualifier for qualifier in feat.qualifiers if qualifier.key == "translation"), None)
-                            if qualifier is None:
-                                rich.print(f"[bold yellow]{'Warning':>12}[/] no 'translation' qualifier found in CDS feature of {record.name!r}")
-                                translation = translate_orf(record.sequence[feat.location.start:feat.location.end])
-                            else:
-                                translation = qualifier.value.rstrip("*")
-                            protein_id = "{}_{}".format(record.name, i)
-                            if protein_id not in protein_sizes:
-                                write_fasta(dst, protein_id, translation)
-                                protein_sizes[protein_id] = len(translation)
-            progress.remove_task(task)
-    progress.console.print(
-        f"[bold green]{'Extracted':>12}[/] {len(protein_sizes)} proteins from {len(representatives)} nucleotide representative"
-    )
-    return protein_sizes
+        Args:
+            gff (typing.Optional[typing.Union[str, pathlib.Path]], optional): Path to the GFF file. Defaults to None.
+        """
+        if gff is not None:
+            self.gff = pathlib.Path(gff)
+        else:
+            self.gff = None
+    def extract_sequences(
+        self,
+        progress: rich.progress.Progress,
+        inputs: typing.List[pathlib.Path],
+        output: pathlib.Path,
+        ): 
+        pass
+
+    def extract_proteins(
+        self,
+        progress: rich.progress.Progress,
+        inputs: typing.List[pathlib.Path],
+        output: pathlib.Path,
+        representatives: typing.Container[str]
+        ): 
+        pass
+
+
